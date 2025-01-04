100d3b77c5efbfe34a0fbbf627e1bd4f2db512ba
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-05 01:34:16 +0500

    Fix File Upload

---
+++ src/app/components/upload-area/upload-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-area.component.html',
+  styles: ``
+})
+export class UploadAreaComponent {
+
+}

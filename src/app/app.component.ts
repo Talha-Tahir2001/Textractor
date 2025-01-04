cb3514e6ac84f3f59b098af3461cbe99720331f5
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-04 23:21:41 +0500

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeToggleComponent } from "./components/theme-toggle/theme-toggle.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { UploadAreaComponent } from "./components/upload-area/upload-area.component";
+import { TypewriterDirective } from './directives/typewriter.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, UploadAreaComponent, TypewriterDirective],
   templateUrl: './app.component.html',
   styles: [],
 })

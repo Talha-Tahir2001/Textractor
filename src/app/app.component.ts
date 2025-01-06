0afe2900d97a486f133007da75e0eff72c5f9dda
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-07 02:05:10 +0500

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ThemeToggleComponent } from "./components/theme-toggle/theme-toggle.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { UploadAreaComponent } from "./components/upload-area/upload-area.component";
-import { TypewriterDirective } from './directives/typewriter.directive';
+import { TextExtractionComponent } from './components/text-extraction/text-extraction.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, UploadAreaComponent, ],
+  imports: [NavbarComponent, UploadAreaComponent, TextExtractionComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'Textractor';
+  extractionData: string | null = null;
+  imagePreview: string | null = null;
+
+  handleTextExtraction(data: string) {
+    this.extractionData = data;
+  }
+
+  handleImageUpload(preview: string) {
+    this.imagePreview = preview;
+  }
 }

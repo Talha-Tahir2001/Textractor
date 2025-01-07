96bd6eda05ea65daee606034d69bebb78c3fcb98
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-08 02:25:11 +0500

    Add Environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_KEY: 'Your Gemini API key',
+    API_KEY: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_KEY: 'Your Gemini API key',
+    API_KEY: '',
 };

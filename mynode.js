7479bde630a5971cca53fd6fc0ea5e0dea69e3c2
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-08 22:58:48 +0500

    Update mynode.js

--- mynode.js
+++ mynode.js
@@ -1,19 +1,32 @@
 import { writeFile } from 'fs';
 import { join } from 'path';
-const successColor = '\x1b[32m%s\x1b[0m';
-const checkSign = '\u{2705}';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+
+// Simulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..'); // Move to the directory
+
+// Load environment variables from .env
 dotenv.config({ path: 'src/.env' });
+
+// Environment file content
 const envFile = `export const environment = {
     API_KEY: '${process.env.API_KEY}',    
 };
 `;
+
+// Define the target path
 const targetPath = join(__dirname, './src/environments/environment.development.ts');
+
+// Write the file
 writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
+        const successColor = '\x1b[32m%s\x1b[0m';
+        const checkSign = '\u{2705}';
         console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
     }
 });
